df46010770b3f3f09833f7141ac2728dd0d0e7d8
Bloodylegend <chase4131@gmail.com>
2016-08-24 09:35:07 -0500

    Create DeleteTemp.ps1

---
+++ Utilities/DeleteTemp.ps1
@@ -0,0 +1,26 @@
+#Clean C:\Windows\Temp
+Get-Job | Remove-Job -Force
+Clear-Host
+# Get Computer names
+$ComputerNames = Get-Content -Path "C:\Users\freemanc.admin\Desktop\DeleteTemp.txt"
+#Clear temp Folder as a job
+$Job = {
+	param([string]$ComputerName)
+
+    Remove-Item "\\$ComputerName\c$\windows\temp\*" -Recurse -verbose -ErrorAction SilentlyContinue 
+}
+#Loop through the computers and run the script block
+foreach($ComputerName in $ComputerNames){
+    Start-Job -ScriptBlock $Job -Name $ComputerName -ArgumentList $ComputerName
+}
+
+while((Get-Job | Where { $_.State -ne "Completed" -and $_.State -ne "Failed" } | Measure).Count -ne 0){
+    Write-Host "Waiting for all jobs to complete"
+
+    Start-Sleep 2
+}
+
+Clear-Host
+
+Write-Host "All jobs complete, showing job list..."
+Get-Job
